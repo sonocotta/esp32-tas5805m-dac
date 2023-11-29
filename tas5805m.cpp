adcb70d11d4173be54c811ae03ec490d09856297
Andriy Malyshenko <amalyshenko@klika-tech.com>
2023-11-29 14:29:30 +0100

    Added volume and gain change. Added fault check and reset. Added mono mode switch

--- tas5805m.cpp
+++ tas5805m.cpp
@@ -6,52 +6,74 @@
 #include "driver/i2c.h"
 #include "tas5805m.hpp"
 
-void tas5805m::begin()
+esp_err_t tas5805m::begin(tas5805m_mode mode)
 {
+    int ret;
+
     /* sound is ready */
     log_d("I2S is ready, setup of the audio amp begin");
     vTaskDelay(1000 / portTICK_RATE_MS);
 
     /* set PDN to 1 */
-    gpio_set_level(TAS5806M_GPIO_PDN, 1);
+    gpio_set_level(TAS5805M_GPIO_PDN, 1);
     vTaskDelay(100 / portTICK_RATE_MS);
 
     log_i("Setting to HI Z");
-    ESP_ERROR_CHECK(_write_byte(0x03, 0x02));
+    ret = _write_byte(TAS5805M_DEVICE_CTRL_2_REGISTER, 0x02);
     vTaskDelay(100 / portTICK_RATE_MS);
+    if (ret != ESP_OK) return ret;
 
     log_i("Setting to PLAY");
-    ESP_ERROR_CHECK(_write_byte(0x03, 0x03));
+    ret = _write_byte(TAS5805M_DEVICE_CTRL_2_REGISTER, 0x03);
+    if (ret != ESP_OK) return ret;
+
+    if (mode == TAS5805M_MONO) {
+
+        uint8_t value = 0;
+        int ret = _read_byte(TAS5805M_DEVICE_CTRL_1_REGISTER, &value);
+        if (ret != ESP_OK) return ret;
+
+        value |= 0b100;
+        log_i("Setting to MONO mode, CTRL_1 = %d", value);        
+        ret = _write_byte(TAS5805M_DEVICE_CTRL_1_REGISTER, value);
+        if (ret != ESP_OK) return ret;
+    }
 
     vTaskDelay(100 / portTICK_RATE_MS);
+    return ret;
+}
 
+esp_err_t tas5805m::getFaultState(uint8_t *h70, uint8_t *h71, uint8_t *h72)
+{
     int ret;
-    uint8_t h70 = 0, h71 = 0, h72 = 0;
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, TAS5806M_ADDRESS << 1 | WRITE_BIT, ACK_CHECK_EN);
-    i2c_master_write_byte(cmd, 0x70, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, TAS5805M_ADDRESS << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, TAS5805M_CHAN_FAULT_REGISTER, ACK_CHECK_EN);
     i2c_master_stop(cmd);
-    ret = i2c_master_cmd_begin(I2C_TAS5806M_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    ret = i2c_master_cmd_begin(I2C_TAS5805M_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
     i2c_cmd_link_delete(cmd);
     if (ret != ESP_OK)
     {
         log_e("I2C ERROR");
     }
+
     vTaskDelay(1 / portTICK_RATE_MS);
     cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, TAS5806M_ADDRESS << 1 | READ_BIT, ACK_CHECK_EN);
-    i2c_master_read_byte(cmd, &h70, ACK_VAL);
-    i2c_master_read_byte(cmd, &h71, ACK_VAL);
-    i2c_master_read_byte(cmd, &h72, NACK_VAL);
+    i2c_master_write_byte(cmd, TAS5805M_ADDRESS << 1 | READ_BIT, ACK_CHECK_EN);
+    i2c_master_read_byte(cmd, h70, ACK_VAL);
+    i2c_master_read_byte(cmd, h71, ACK_VAL);
+    i2c_master_read_byte(cmd, h72, NACK_VAL);
     i2c_master_stop(cmd);
-    ret = i2c_master_cmd_begin(I2C_TAS5806M_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    ret = i2c_master_cmd_begin(I2C_TAS5805M_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
     i2c_cmd_link_delete(cmd);
+    return ret;
+}
 
-    log_d("0x70 Register: %d", h70);
-    log_d("0x71 Register: %d", h71);
-    log_d("0x72 Register: %d", h72);
+esp_err_t tas5805m::clearFaultState() 
+{
+    return _write_byte(TAS5805M_FAULT_CLEAR_REGISTER, (uint8_t)0b10000000);
 }
 
 esp_err_t tas5805m::init()
@@ -60,22 +82,15 @@ esp_err_t tas5805m::init()
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = TAS5806M_GPIO_PDN_MASK;
+    io_conf.pin_bit_mask = TAS5805M_GPIO_PDN_MASK;
     io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
     io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    log_d("Power down pin: %d", TAS5805M_GPIO_PDN); 
     gpio_config(&io_conf);
-    gpio_set_level(TAS5806M_GPIO_PDN, 0);
-    vTaskDelay(20 / portTICK_RATE_MS);
-    gpio_set_level(TAS5806M_GPIO_PDN, 1);
+    gpio_set_level(TAS5805M_GPIO_PDN, 0);
     vTaskDelay(200 / portTICK_RATE_MS);
-
-    /* initialize I2C to drive the TAS5806M */
-    // ret = i2c_master_init();
-    // if (ret != ESP_OK)
-    // {
-    //     ESP_LOGE(TAS5806M_TAG, "I2C could not be initialized.");
-    //     return ret;
-    // }
+    gpio_set_level(TAS5805M_GPIO_PDN, 1);
+    vTaskDelay(200 / portTICK_RATE_MS);
 
     return ESP_OK;
 }
@@ -86,32 +101,96 @@ esp_err_t tas5805m::_read(uint8_t register_name, uint8_t *data_rd, size_t size)
     {
         return ESP_OK;
     }
+
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (TAS5806M_ADDRESS << 1) | READ_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, (TAS5805M_ADDRESS << 1) | READ_BIT, ACK_CHECK_EN);
     i2c_master_write_byte(cmd, register_name, ACK_CHECK_EN);
+
     if (size > 1)
     {
         i2c_master_read(cmd, data_rd, size - 1, ACK_VAL);
     }
     i2c_master_read_byte(cmd, data_rd + size - 1, NACK_VAL);
     i2c_master_stop(cmd);
-    esp_err_t ret = i2c_master_cmd_begin(I2C_TAS5806M_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    esp_err_t ret = i2c_master_cmd_begin(I2C_TAS5805M_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
     i2c_cmd_link_delete(cmd);
+
     return ret;
 }
+
+
+esp_err_t tas5805m::_read_byte(uint8_t register_name, uint8_t *data)
+{
+    
+    int ret;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, TAS5805M_ADDRESS << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, register_name, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(I2C_TAS5805M_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    
+    if (ret != ESP_OK)
+    {
+        log_e("I2C ERROR");
+    }
+
+    vTaskDelay(1 / portTICK_RATE_MS);
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, TAS5805M_ADDRESS << 1 | READ_BIT, ACK_CHECK_EN);
+    i2c_master_read_byte(cmd, data, NACK_VAL);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(I2C_TAS5805M_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    
+    return ret;
+}
+
 
 esp_err_t tas5805m::_write_byte(uint8_t register_name, uint8_t value)
 {
     esp_err_t ret;
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, TAS5806M_ADDRESS << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, TAS5805M_ADDRESS << 1 | WRITE_BIT, ACK_CHECK_EN);
     i2c_master_write_byte(cmd, register_name, ACK_CHECK_EN);
     i2c_master_write_byte(cmd, value, ACK_CHECK_EN);
     i2c_master_stop(cmd);
-    ret = i2c_master_cmd_begin(I2C_TAS5806M_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    ret = i2c_master_cmd_begin(I2C_TAS5805M_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
     i2c_cmd_link_delete(cmd);
 
     return ret;
+}
+
+esp_err_t tas5805m::getGain(uint8_t* volume)
+{
+    uint8_t value = 0;
+    int ret = _read_byte(TAS5805M_AGAIN_REGISTER, &value);
+    *volume = value << 3;
+    return ret;
+}
+
+// 0-255, where 0 = 0 Db, 255 = -15.5 Db
+esp_err_t tas5805m::setGain(uint8_t value)
+{
+    uint8_t volume = value >> 3;
+    return _write_byte(TAS5805M_AGAIN_REGISTER, volume);
+}
+
+esp_err_t tas5805m::getVolume(uint8_t* volume)
+{
+    uint8_t value = 0;
+    int ret = _read_byte(TAS5805M_DIG_VOL_CTRL_REGISTER, &value);
+    *volume = value;
+    return ret;
+}
+
+// 0-255, where 0 = 0 Db, 255 = mute
+esp_err_t tas5805m::setVolume(uint8_t value)
+{
+    uint8_t volume = value;
+    return _write_byte(TAS5805M_DIG_VOL_CTRL_REGISTER, volume);
 }
